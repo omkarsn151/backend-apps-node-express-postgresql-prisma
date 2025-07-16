c797a81c69a8b36a0e707482ad4aeb1bc0d6f288
omkarsn151 <omkarsnimbalkar1501@gmail.com>
2025-07-17 00:06:36 +0530

    first commit

---
+++ db_connection/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import "dotenv/config.js";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.get('/', (req, res) => {
+  res.send('🚀 API is running...');
+});
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+
+export default app;
