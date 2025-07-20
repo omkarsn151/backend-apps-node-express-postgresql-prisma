9c46be6c33ba1169ed99789116c97231755fecba
omkarsn151 <omkarsnimbalkar1501@gmail.com>
2025-07-20 18:12:38 +0530

    controller to get all notes

--- crud_app/src/controllers/notes.controller.js
+++ crud_app/src/controllers/notes.controller.js
@@ -1,8 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const prisma = new PrismaClient();
+
 const getNotes = asyncHandler( async (req, res) => {
+    const notes = await prisma.notes.findMany();
     res.status(200).json({
-        message: "OK"
+        message: "OK",
+        data: notes
     })
 })
 
