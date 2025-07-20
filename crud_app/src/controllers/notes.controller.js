274b6bb6b0cf94220b3da9f4e84c8956ed5428ac
omkarsn151 <omkarsnimbalkar1501@gmail.com>
2025-07-20 18:30:53 +0530

    api to create a new note

--- crud_app/src/controllers/notes.controller.js
+++ crud_app/src/controllers/notes.controller.js
@@ -3,6 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const prisma = new PrismaClient();
 
+// get-notes
 const getNotes = asyncHandler( async (req, res) => {
     const notes = await prisma.notes.findMany();
     res.status(200).json({
@@ -11,5 +12,26 @@ const getNotes = asyncHandler( async (req, res) => {
     })
 })
 
+// add-note
+const addNote = asyncHandler( async (req, res) => {
+    const { title, description } = req.body;
 
-export {getNotes}
+    if (!title) {
+        res.status(400).json({
+            message: "Title is required"
+        })
+    }
+
+    const newNote = await prisma.notes.create({
+        data : {
+            title,
+            description,
+        }
+    });
+    res.status(201).json({
+        message: "New note added Successfully!",
+        data: newNote
+    });
+})
+
+export {getNotes, addNote}
--- crud_app/src/routes/notes.routes.js
+++ crud_app/src/routes/notes.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getNotes } from "../controllers/notes.controller.js";
+import { addNote, getNotes } from "../controllers/notes.controller.js";
 
 const router = Router()
 
+// get-all-notes
 router.route('/get-notes').get(getNotes)
+
+// add-note
+router.route('/add-note').post(addNote)
 
 export default router
